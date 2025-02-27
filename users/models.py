982de255c4e2afa1da1b3635a1d4cf76139821f1
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-02-27 18:53:28 +0400

    fix user

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
     def __str__(self):
         return self.username
